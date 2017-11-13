35509041eae6023d7921437f8b2d3a3e1b625690
liudong <1418275530@qq.com>
2017-11-13 18:18:07 +0800

    动态添加组件

---
+++ src/components/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test">
+      <el-form ref="form" :model="form" label-width="80px" >
+        <el-form-item label="活动名称">
+          <el-input  v-model="form.name" style="width:200px"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+      }
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/KMS/classify1.vue
+++ src/views/KMS/classify1.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="classify1">
-      分类1
+      <el-form ref="form" :model="form" label-width="80px" >
+        <el-button type="primary" @click="add('Test')">+</el-button>
+        <div :is="item.component" v-for="item in items">{{item}}</div>
+        <el-form-item label="活动时间">
+          <el-col :span="6">
+            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="1">-</el-col>
+          <el-col :span="6">
+            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
   </div>
 </template>
 
 <script>
+import Test from './../../components/test'
 export default {
-  name:"classify1",
+  name: "classify1",
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+      },
+      items:[]
+    };
+  },
+  components:{
+    Test
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+    add(components){
+      alert(125);
+      this.items.push({
+        'component': components,
+      })
 
-}
+    }
+  }
+};
 </script>
 
 <style>
